111435ae8b8d94530afbdf2b1e84dbbc9c479eea
Bagus Adianto <bagusadianto5@gmail.com>
2023-12-12 18:59:45 +0700

    Update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('be_2a', 'railway', '', {
-    host: 'railway',
+    host: 'roundhouse.proxy.rlwy.net',
+    port: 3306,
     dialect: 'mysql'
 });
 
